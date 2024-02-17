bad1df520e1482d7d56b67d1d5568a9b7bfb827f
b6dbde <b6dbde@gmail.com>
2024-02-17 19:05:59 +0700

    added method for proceeding views

--- src/Core/Controller.php
+++ src/Core/Controller.php
@@ -5,4 +5,29 @@ namespace App\Core;
 abstract class Controller
 {
     public abstract function index();
+
+    protected static function isUserLogged()
+    {
+        return  isset($_SESSION['logged_user_id']) &&
+                !empty($_SESSION['logged_user_id'])
+                ;
+    }
+
+    protected static function redirect(string $to)
+    {
+        header("Location:$to");
+        die();
+    }
+
+    protected static function refresh(int $in = 0)
+    {
+        header("Refresh:$in");
+        die();
+    }
+
+    protected function proceedView(string $filename, array $viewData)
+    {
+        extract($viewData);
+        require Config::$VIEWS_PATH . $filename . ".php";
+    }
 }
